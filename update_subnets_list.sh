950cd8c84f30f694ebcb1e75f00894a5c467ac7a
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2025-06-07 06:51:58 +0300

    visual

--- update_subnets_list.sh
+++ update_subnets_list.sh
@@ -52,10 +52,10 @@ find ${FOLDER} -type f -name "*.yaml" -exec rm -f {} +
 main() {
 	download "https://www.cloudflare.com/ips-v4" ${FOLDER}ipv4/cloudflare.txt
 	download "https://www.cloudflare.com/ips-v6" ${FOLDER}ipv6/cloudflare.txt
-	download "https://core.telegram.org/resources/cidr.txt" ${FOLDER}dual/telegram.txt
 	download "https://iplist.opencck.org/?format=text&data=cidr4&site=discord.gg&site=discord.media" ${FOLDER}ipv4/discord.txt
 	download "https://iplist.opencck.org/?format=text&data=cidr6&site=discord.gg&site=discord.media" ${FOLDER}ipv6/discord.txt
 
+    download "https://core.telegram.org/resources/cidr.txt" ${FOLDER}dual/telegram.txt
 	split_subnets ${FOLDER}dual/telegram.txt ${FOLDER}ipv4/telegram.txt ${FOLDER}ipv6/telegram.txt
 
 	echo >> ${FOLDER}ipv4/cloudflare.txt
