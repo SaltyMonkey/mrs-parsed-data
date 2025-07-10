7e78444c996fa979a7d3cd022e2addb004e51181
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2025-07-11 02:26:30 +0300

    fixes + better look for list

--- update_readme.sh
+++ update_readme.sh
@@ -1,62 +1,71 @@
 #!/bin/bash
-GITHUBLINK="https://github.com/SaltyMonkey/mrs-parsed-data/raw/refs/heads/main"
+GITHUBLINK="https://github.com/SaltyMonkey/rms-parsed-data/raw/refs/heads/main"
 
 rm -f ./README.md
 
-SERVICES_MRS=$(find "./services" -type f -name "*.mrs" | while read -r file; do
+SERVICES_rms=$(find "./services" -type f -name "*.rms" | while read -r file; do
+    file_name=$(basename "$file")
     file_path=${file#./}
-    echo "- [$file_path]($GITHUBLINK/$file_path)"
+    echo "- [$file_name]($GITHUBLINK/$file_path)"
 done)
 SERVICES_YAML=$(find "./services" -type f -name "*.yaml" | while read -r file; do
+    file_name=$(basename "$file")
     file_path=${file#./}
-    echo "- [$file_path]($GITHUBLINK/$file_path)"
+    echo "- [$file_name]($GITHUBLINK/$file_path)"
 done)
-BYPASS_MRS=$(find "./bypass" -type f -name "*.mrs" | while read -r file; do
+BYPASS_rms=$(find "./bypass" -type f -name "*.rms" | while read -r file; do
+    file_name=$(basename "$file")
     file_path=${file#./}
-    echo "- [$file_path]($GITHUBLINK/$file_path)"
+    echo "- [$file_name]($GITHUBLINK/$file_path)"
 done)
 BYPASS_YAML=$(find "./bypass" -type f -name "*.yaml" | while read -r file; do
+    file_name=$(basename "$file")
     file_path=${file#./}
-    echo "- [$file_path]($GITHUBLINK/$file_path)"
+    echo "- [$file_name]($GITHUBLINK/$file_path)"
 done)
-ADS_MRS=$(find "./ads" -type f -name "*.mrs" | while read -r file; do
+ADS_rms=$(find "./ads" -type f -name "*.rms" | while read -r file; do
+    file_name=$(basename "$file")
     file_path=${file#./}
-    echo "- [$file_path]($GITHUBLINK/$file_path)"
+    echo "- [$file_name]($GITHUBLINK/$file_path)"
 done)
 ADS_YAML=$(find "./ads" -type f -name "*.yaml" | while read -r file; do
+    file_name=$(basename "$file")
     file_path=${file#./}
-    echo "- [$file_path]($GITHUBLINK/$file_path)"
+    echo "- [$file_name]($GITHUBLINK/$file_path)"
 done)
-BADWARE_MRS=$(find "./badware" -type f -name "*.mrs" | while read -r file; do
+BADWARE_rms=$(find "./badware" -type f -name "*.rms" | while read -r file; do
+    file_name=$(basename "$file")
     file_path=${file#./}
-    echo "- [$file_path]($GITHUBLINK/$file_path)"
+    echo "- [$file_name]($GITHUBLINK/$file_path)"
 done)
 BADWARE_YAML=$(find "./badware" -type f -name "*.yaml" | while read -r file; do
+    file_name=$(basename "$file")
     file_path=${file#./}
-    echo "- [$file_path]($GITHUBLINK/$file_path)"
+    echo "- [$file_name]($GITHUBLINK/$file_path)"
 done)
-SUBNETS_MRS=$(find "./subnets" -type f -name "*.mrs" | while read -r file; do
+SUBNETS_rms=$(find "./subnets" -type f -name "*.rms" | while read -r file; do
+    file_name=$(basename "$file")
     file_path=${file#./}
-    echo "- [$file_path]($GITHUBLINK/$file_path)"
+    echo "- [$file_name]($GITHUBLINK/$file_path)"
 done)
 
 cat <<EOF >> ./README.md
 ## Services
-$SERVICES_MRS
+$SERVICES_rms
 $SERVICES_YAML
 
 ## ADS
-$ADS_MRS
+$ADS_rms
 $ADS_YAML
 
 ## Badware
-$BADWARE_MRS
+$BADWARE_rms
 $BADWARE_YAML
 
 ## Bypass
-$BYPASS_MRS
+$BYPASS_rms
 $BYPASS_YAML
 
 ## Subnets
-$SUBNETS_MRS
+$SUBNETS_rms
 EOF
